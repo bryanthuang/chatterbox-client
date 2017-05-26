ddedc2908cbf82a6c271a10ac62d382d87995b3d
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-05-26 12:30:44 -0700

    Finished first three tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,13 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.init = function() {};
+app.send = function() {
+  $.ajax({
+    type: "POST",
+    url: "http://parse.sfm6.hackreactor.com/chatterbox/classes/messages",
+    //data: data,
+    //success: success,
+    //dataType: dataType
+  });
+};
