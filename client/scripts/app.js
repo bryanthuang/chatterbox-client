33b9db27c81b869023f2a25d1ebeae258e5eda90
Hack Reactor Student <student@hackreactor.com>
2017-05-26 17:40:41 -0700

    lots of progress

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,16 +3,22 @@ var app = {
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
 };
 
-app.init = function() {};
+app.init = function() {
+  $(document).ready(function () {
+    app.fetch();
+
+  });
+};
 
 app.send = function(message) {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
-    data: JSON.stringify(message),
+    data: message,
     contentType: 'application/json',
     success: function (data) {
+      console.log(data);
       console.log('chatterbox: Message sent');
     },
     error: function (data) {
@@ -27,22 +33,35 @@ app.fetch = function() {
     // This is the url you should use to communicate with the parse API server.
     url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
-    data: JSON.stringify(message),
+    data : 'order=-createdAt',
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      console.log('chatterbox: Message received');
+      console.log(data);
+      
+      for (var i = 0; i < 10; i++) {
+        // console.log(JSON.stringify(data.results[i]));
+        app.renderMessage(JSON.stringify(data.results[i].text), data.results[i].username, i);
+      }
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
+      console.error('chatterbox: Failed to retrieve message', data);
     }
   });
 };
 
 app.clearMessages = function () {
-  $('#chats').remove();
+  $('#chats').children().remove();
 };
 
-app.renderMessage = function(message) {
-  $('#chats').append(message);
+app.renderMessage = function(message, username, index) {
+  $('#chats').prepend(`<div><p class="username">${index}${username} :</p><p class="chat">${message}</p></div>`);
+  
 };
+
+app.renderRoom = function(roomName) {
+  $('#roomSelect').wrapInner(`<option value=${roomName}>${roomName}</option>`);
+};
+
+app.init();
